164b4ca071b6bbf6e9733a22bba77c4826f53067
Francisco Entony <franciscoentonydev@gmail.com>
2026-08-06 18:23:32 +0000

    feat(client): criando página `Em Construção`

---
+++ src/_components/UnderConstruction.js
@@ -0,0 +1,8 @@
+export default function UnderConstruction() {
+  return (
+    <>
+      <img src="/identity/co.png" alt="logo" width={100} />
+      <h1 className="text-xl font-semibold">Em Construção...</h1>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import UnderConstruction from "@/_components/UnderConstruction";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full items-center justify-center py-32 px-16">
-        <h1 className="text-2xl font-semibold">Connect</h1>
+      <main className="flex flex-col gap-5 w-full items-center justify-center py-32 px-16">
+        <UnderConstruction />
       </main>
     </div>
   );
