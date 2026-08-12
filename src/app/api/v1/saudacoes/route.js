ffe768a476e0d2d90ba11a34254b4c3daf8a8f57
Francisco Entony <franciscoentonydev@gmail.com>
2026-08-12 02:47:55 +0000

    feat(api): adicionando hora atual ao endpoint `/saudacoes`

--- src/app/api/v1/saudacoes/route.js
+++ src/app/api/v1/saudacoes/route.js
@@ -1,5 +1,10 @@
 import { NextResponse } from "next/server";
+import { query } from "infra/database.js";
 
 export async function GET() {
-  return NextResponse.json({ msg: "Fala dev!" });
+  const result = await query("SELECT NOW()");
+  return NextResponse.json({
+    msg: "Fala dev!",
+    hora_atual: result.rows[0].now,
+  });
 }
